76a79515c03093d6eb145ba5eb7b4289d8818353
filecc <fil.gia@live.it>
2023-03-02 19:10:35 +0100

    readme file

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function validateForm() {
-    let x = document.forms["email_form"]["input_email"].value;
-    if (!x.includes('@') || x=="") {
-      let input = document.getElementById('invalid');
-      input.classList.toggle('hidden');
-      let submit = document.getElementById('submit');
-      submit.classList.toggle('mt-3');
-      submit.classList.toggle('mt-1');
-      return false;
-    }
+  let x = document.forms["email_form"]["input_email"].value;
+  let submit = document.getElementById("submit");
+  let message = document.getElementById("invalid-text");
+  if (x == "") {
+    message.classList.toggle("hidden");
+    submit.classList.toggle("mt-3");
+    submit.classList.toggle("mt-1");
+    message.innerHTML= "Oops! Please add your email";
+    return false;
+  } else if (!x.includes('@') && x!="") {
+    message.classList.toggle("hidden");
+    submit.classList.toggle("mt-3");
+    submit.classList.toggle("mt-1");
+
+    message.innerHTML="Oops! Please check your email";
+    return false;
   }
+}
